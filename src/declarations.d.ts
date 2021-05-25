07bb98e170e69d450934ae96b66bbcdd2110c0c5
RayBDev <raybernard1337@gmail.com>
2021-05-25 07:26:53 -0500

    Added Typescript config

---
+++ src/declarations.d.ts
@@ -0,0 +1,12 @@
+// This file is used to hold ambient type declarations, as well as type shims
+// for npm module without type declarations, and assets files.
+
+// For example, to shim modules without declarations, use:
+// declare module "package-without-declarations"
+declare module "*.svg" {
+  const content: any
+  export default content
+}
+
+// And to shim assets, use (one file extension per `declare`):
+// declare module "*.png"
